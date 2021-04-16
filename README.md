59c4589f62a8d24fbb4dfa00c206a76be1415358
inspier <devdevin541@gmail.com>
2021-04-16 12:23:32 -0500

    lib: Allow concatenation in non-const contexts.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 # array-concat
 
-Macros for concatenating const arrays at compile time.
+Macros for concatenating const arrays.
 
 To add to your Cargo.toml:
 ```toml
@@ -12,19 +12,21 @@ array-concat = "0.1.0"
 
 ## Example
 ```rust
-use array_concat::{concat_arrays_size, concat_arrays};
+use array_concat::*;
 
 const A: [u32; 3] = [1, 2, 3];
 const B: [u32; 2] = [4, 5];
-const C: [u32; concat_arrays_size!(A, B)] = concat_arrays!(u32; A, B; u32::MIN); // compiles
+const C: [u32; concat_arrays_size!(A, B)] = concat_arrays_const!(u32; A, B; u32::MIN); // compiles
 
 // Non-Copy struct
 struct S {}
 const D: [S; 1] = [S{}];
 const E: [S; 1] = [S{}];
-const F: [S; concat_arrays_size!(D, E)] = concat_arrays!(S; D, E; S{}); // doesn't compile
+const F: [S; concat_arrays_size!(D, E)] = concat_arrays_const!(S; D, E; S{}); // doesn't compile
 
 fn main() {
+    let c = concat_arrays!(u32; A, B; u32::MIN);
     assert_eq!(C[..], A.iter().chain(&B).copied().collect::<Vec<u32>>()[..]);
+    assert_eq!(c[..], A.iter().chain(&B).copied().collect::<Vec<u32>>()[..]);
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,11 @@ macro_rules! concat_arrays_size {
     }};
 }
 
-/// Concatenates provided const arrays.
+/// Concatenates provided arrays.
 #[macro_export]
 macro_rules! concat_arrays {
     ($t:ty; $( $array:tt ),*; $init_value:expr) => ({
         const __ARRAY_SIZE__: usize = $crate::concat_arrays_size!($($array),*);
-        const __CONCAT__: [$t; __ARRAY_SIZE__] = {
         let mut result = [$init_value; __ARRAY_SIZE__];
 
         #[allow(non_snake_case)]
@@ -33,8 +32,17 @@ macro_rules! concat_arrays {
         result = c.full;
         }
         result
-        };
-        __CONCAT__
+    });
+}
+
+/// Concatenates provided arrays in const context.
+#[macro_export]
+macro_rules! concat_arrays_const {
+    ($t:ty; $( $array:tt ),*; $init_value:expr) => ({
+    const __CONCAT__: [$t; $crate::concat_arrays_size!($($array),*)] = {
+    concat_arrays!($t; $($array),*; $init_value)
+    };
+    __CONCAT__
     });
 }
 
@@ -43,19 +51,23 @@ macro_rules! concat_arrays {
 mod tests {
     use super::*;
 
+    const A: [u32; 3] = [1, 2, 3];
+    const B: [u32; 3] = [4, 5, 6];
+    const C: [u32; 2] = [4, 5];
+
     #[test]
     fn test_simple_concat() {
-        const A: [u32; 3] = [1, 2, 3];
-        const B: [u32; 3] = [4, 5, 6];
-        const C: [u32; concat_arrays_size!(A, B)] = concat_arrays!(u32; A, B; u32::MIN);
-        assert_eq!([1, 2, 3, 4, 5, 6], C);
+        let d = concat_arrays!(u32; A, B; u32::MIN);
+        const D: [u32; concat_arrays_size!(A, B)] = concat_arrays!(u32; A, B; u32::MIN);
+        assert_eq!([1, 2, 3, 4, 5, 6], D);
+        assert_eq!([1, 2, 3, 4, 5, 6], d);
     }
 
     #[test]
     fn test_different_sizes() {
-        const A: [u32; 3] = [1, 2, 3];
-        const B: [u32; 2] = [4, 5];
-        const C: [u32; concat_arrays_size!(A, B)] = concat_arrays!(u32; A, B; u32::MIN);
-        assert_eq!([1, 2, 3, 4, 5], C);
+        let e = concat_arrays!(u32; A, C; u32::MIN);
+        const E: [u32; concat_arrays_size!(A, C)] = concat_arrays!(u32; A, C; u32::MIN);
+        assert_eq!([1, 2, 3, 4, 5], E);
+        assert_eq!([1, 2, 3, 4, 5], e);
     }
 }
